166b9a73962d09b37f4bd57337faf767586d627c
Guanhua <william.ting.1116@gmail.com>
2019-03-04 12:05:21 +1100

    basic layout

---
+++ src/components/NewNote.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class NewNote extends Component {
+  render () {
+    return(
+      <div className="cardThumbnail">
+        <div className="card">
+          <div className="card-body">
+            <h5>+</h5>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+// Bootstrap
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
