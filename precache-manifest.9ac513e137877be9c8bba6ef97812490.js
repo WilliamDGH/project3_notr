6f1fd3d60ea1b5b08bc1327bdb59935a085cfbd5
Guanhua <william.ting.1116@gmail.com>
2019-03-06 13:58:36 +1100

    Updates

---
+++ precache-manifest.9ac513e137877be9c8bba6ef97812490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f563be0d0db5fee3856",
+    "url": "/project3_notr/static/css/main.c4f9fbf6.chunk.css"
+  },
+  {
+    "revision": "6f563be0d0db5fee3856",
+    "url": "/project3_notr/static/js/main.6f563be0.chunk.js"
+  },
+  {
+    "revision": "e2b98c18ae7cbfb5640d",
+    "url": "/project3_notr/static/js/runtime~main.e2b98c18.js"
+  },
+  {
+    "revision": "575e47ee8e76fde04bb5",
+    "url": "/project3_notr/static/css/2.857f8095.chunk.css"
+  },
+  {
+    "revision": "575e47ee8e76fde04bb5",
+    "url": "/project3_notr/static/js/2.575e47ee.chunk.js"
+  },
+  {
+    "revision": "6443d8f330561ed7c398eb9bf068a889",
+    "url": "/project3_notr/index.html"
+  }
+];
